e21d1a0797d83ce5ece4b9c9a1bfeb0b0705f1e0
VaishnaviG77 <101456565+VaishnaviG77@users.noreply.github.com>
2024-06-03 15:48:46 +0530

    Create input_output.js

---
+++ Variables, Input-Output/input_output.js
@@ -0,0 +1,8 @@
+//Write a program to take input from the user, and then print it to the output.
+
+//Flow: input a --> print a
+
+const input = require("readline-sync")
+let a = input.question("Enter your input: ")
+
+console.log(a)
